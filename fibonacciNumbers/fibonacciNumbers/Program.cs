d1d498dea5849596071fddc86b859ad50ab64849
Divya Shivaprakashan <shivaprakashd8313@uhcl.edu>
2020-09-26 09:35:09 -0500

    fib

---
+++ fibonacciNumbers/fibonacciNumbers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace fibonacciNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                       Console.WriteLine("Enter the last number of Fibonacci Sequence of numbers.");
+                int end = int.Parse(Console.ReadLine());
+
+
+                int input, first = 0, second = 1, third = 0;
+                Console.Write("Enter a number : ");
+                n = Convert.ToInt32(Console.ReadLine());
+                Console.Write("First {0} Fibonacci numbers {1} {2} ", input, first, second);
+
+                for (int i = 3; i <= input; i++)
+                {
+                    third = first + second;
+                    Console.Write("{0} ", third);
+                    first = second;
+                    second = third;
+                }
+
+
+            }
+
+        }
+    }
+}
+    }
+}
