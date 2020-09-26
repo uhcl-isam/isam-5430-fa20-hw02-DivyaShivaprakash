271ba23bea2d6defd700d28d9f0d2cbed9e8c601
Divya Shivaprakashan <shivaprakashd8313@uhcl.edu>
2020-09-26 09:42:47 -0500

    intpalindromes

---
+++ Int/Int/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Int
+{
+     class IntegerPalindromes
+    {
+        public static void Main()
+        {
+            // enter sentinel inputs
+            // then check the palindrome only once.
+
+            int n, r, sum = 0, temp;
+            Console.Write("Enter the Number: ");
+            n = int.Parse(Console.ReadLine());
+            temp = n;
+            while (n > 0)
+            {
+                r = n % 10;
+                sum = (sum * 10) + r;
+                n = n / 10;
+            }
+            if (temp == sum)
+                Console.Write("Number is Palindrome.");
+            else
+                Console.Write("Number is not Palindrome");
+        }
+    }
+}
+
