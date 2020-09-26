eb5373acb13a38b9e93f9f19d17fd8761da9e763
Divya Shivaprakashan <shivaprakashd8313@uhcl.edu>
2020-09-26 09:40:55 -0500

    positive

---
+++ PositiveInts/PositiveInts/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp.Assignments.Loop1
+{
+    public class PositiveInts
+    {
+        /// <summary>
+        /// First prompt for the value of n, then Write all the positive integers up to the integer n on separate lines. [Demonstration]
+        /// </summary>
+        public static void Main()
+        {
+            Console.Error.WriteLine("Enter the value of n");
+            // write your codes here.
+
+            int num;
+            Console.Write("\n\n");
+            Console.Write("Check whether a number is positive or negative:\n");
+            Console.Write("----------------------------------------------");
+            Console.Write("\n\n");
+            Console.Write("Input an integer : ");
+            num = Convert.ToInt32(Console.ReadLine());
+            if (num >= 0)
+
+                Console.WriteLine("{0} is a positive number.\n", num);
+            else
+                Console.WriteLine("{0} is a negative number. \n", num);
+        }
+    }
+}
