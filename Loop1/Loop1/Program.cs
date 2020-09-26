3e1cdbe73a3ea45001769149b86198c693d84f93
Divya Shivaprakashan <shivaprakashd8313@uhcl.edu>
2020-09-26 12:11:26 -0500

    loop1

---
+++ Loop1/Loop1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.Assignments.Loop1
+{
+    public class AscendingFillers
+    {
+        public static void Main()
+        {
+            int pn = 0, filler = 0, sum = 0;
+
+            while (true)
+            {
+                Console.WriteLine("Enter non negative integers");
+                filler = 0;
+                string cnn = Console.ReadLine();
+                int cn = Convert.ToInt32(cnn);
+                if ((cn < 0) | (cnn == null))
+                {
+                    break;
+                }
+
+                else if (cn <= pn && cn != 0)
+                {
+                    filler = (pn + 1) - cn;
+                    cn = pn + 1;
+                }
+
+                pn = cn;
+                sum += filler;
+                //Console.WriteLine(sum);
+                //Console.WriteLine(filler);
+
+            }
+            Console.WriteLine(sum);
+
+        }
+    }
+}
