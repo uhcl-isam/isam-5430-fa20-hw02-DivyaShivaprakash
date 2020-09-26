3e7b8835db6a48d7094f1f7d9e0fc057ae43e91e
Divya Shivaprakashan <shivaprakashd8313@uhcl.edu>
2020-09-26 09:17:57 -0500

    excersise

---
+++ CheckerboardPattern/CheckerboardPattern/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CheckerboardPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
